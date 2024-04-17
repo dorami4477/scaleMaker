c4240f043af93acebe2d02eb444fa6779e1b1f7a
dorami4477 <dahyun4477@gmail.com>
2024-04-17 11:57:56 +0900

    Initial Commit

---
+++ scaleMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scaleMaker
+//
+//  Created by 박다현 on 4/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
